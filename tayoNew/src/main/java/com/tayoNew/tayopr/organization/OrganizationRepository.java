476575deac1653cf21756f1ef8f7e6eae2b5c143
minseong1017 <150658113+minseong1017@users.noreply.github.com>
2024-06-26 09:43:26 +0900

    Delete tayoNew directory

--- tayoNew/src/main/java/com/tayoNew/tayopr/organization/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tayoNew.tayopr.organization;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizationRepository extends JpaRepository<Organization,Long>{
-	Optional<Organization> findByorganizationName(String organizationName);
-}
--- tayoNew/src/main/resources/static/Regist.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const agreeChkAll = document.querySelector('input[name=agree_all]');
-    agreeChkAll.addEventListener('change', (e) => {
-        let agreeChk = document.querySelectorAll('input[name=agree]');
-        for(let i = 0; i < agreeChk.length; i++){
-            agreeChk[i].checked = e.currentTarget.checked;
-        }
-    });
-});
